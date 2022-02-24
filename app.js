b5ad229ef2069bc9ad83cd25732ca51c991e6879
Julian <julian_delisio@hotmail.com>
2022-02-23 23:24:33 -0300

    Se genera estructura de proyecto y conexión a db

---
+++ app.js
@@ -0,0 +1,41 @@
+
+//1- Invocamos a express
+const express = require('express');
+const app = express();
+
+//2- Seteamos urlencoded para capturar los datos del formulario sin errores   
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//3- Invocamos DotEnv para leer las variables de entorno
+const dotenv = require('dotenv');
+dotenv.config({path: "./env/.env"});
+
+//4- Seteamos el directorio public
+app.use("/resorces", express.static("/public"));
+app.use("/resorces", express.static(__dirname + "/public"));
+
+//5- Establecemos el motor de plantillas
+app.set("view engine", "ejs");
+
+//6- Invocamos el modulo para el hashing del password
+const bcryptjs = require("bcryptjs");
+
+//7- Variables de session
+const session = require("express-session");
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+
+//8- Invocamos el módulo de conexión de la base de datos
+const conection = require("./database/db");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');  
+});
+
+app.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000");
+});
